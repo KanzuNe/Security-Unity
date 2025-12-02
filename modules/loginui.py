3ed1675232229b050282c78fb9ef89bfa1180a21
KanzuNe <hieu04092007@gmail.com>
2025-12-02 22:36:00 +0700

    adding how to link ui and stuff

--- modules/loginui.py
+++ modules/loginui.py
@@ -5,13 +5,30 @@ from PyQt5 import uic
 from PyQt5.QtCore import Qt
 import json
 
+class Users:
+     pass_list =[]
+     def __init__(self,website, username, password):
+          self.website = website
+          self.username = username
+          self.password = password
+
 class MainUi(QMainWindow):
-    def __init__(self):
+    def __init__(self, username):
         super().__init__()
         uic.loadUi(r"ui\passui.ui", self)
+        self.current_user = username
+        self.setWindowTitle(f"Password Manager - {username}")
+        load_pass(username)
+        self.update_table()
 
-
-
+    def update_table(self):
+        self.table.setRowCount(len(Users.pass_list))
+        for index, f in enumerate(Users.pass_list):
+             self.table.setItem(index, 0, QTableWidgetItem(f.website))
+             self.table.setItem(index, 1, QTableWidgetItem(f.username))
+             self.table.setItem(index, 2, QTableWidgetItem(f.password))
+        
+        
 class LoginUi(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -48,10 +65,26 @@ class LoginUi(QMainWindow):
         current_username = self.UserChoice.currentText()
         password = self.PassEnter.text()
         if (current_username, password) in self.user_list.items():
-                self.mainui = MainUi()
+                self.mainui = MainUi(current_username)
                 self.mainui.show()
-                self.hide()
+                self.hide()                
 
+
+
+          
+def load_pass(username):
+        CSV_FILE= f"data/vaults/{username}.csv"
+        with open (CSV_FILE, "r", encoding="utf-8") as f:
+            for line in f:
+                line = line.strip()
+                if line:
+                    website, username_entry, password = line.split(",")
+                    Users.pass_list.append(Users(website, username_entry, password))
+def save_pass(username):
+        CSV_FILE= f"data/vaults/{username}.csv"
+        with open (CSV_FILE, "w", encoding="utf-8") as f:
+            for num in Users.pass_list:
+                f.write(f"{num.website},{num.username},{num.password}\n")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
